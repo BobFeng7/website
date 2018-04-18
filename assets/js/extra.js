d2ede937fd2d76402e1e1d2bf5013f56ca5ffb24
Bob Feng <bob.feng.com@gmail.com>
2018-04-17 22:33:03 -0700

    small edits 4/17

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -1,40 +1,53 @@
-window.onload = function() {
-	var vid = document.getElementById("myVideo");
-	vid.playbackRate = 0.75;
+// window.onload = function() {
+// 	var vid = document.getElementById("myVideo");
+// 	vid.playbackRate = 0.75;
+// }
 
-}
 
-// handles the photoreel
-var myIndex = 0;
-carousel();
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 0}    
-    x[Math.floor(myIndex * Math.random() * 20) % x.length].style.display = "block";  
-    setTimeout(carousel, 9000);    
-}
 
-//handles the skills slider 
-var waypoint = new Waypoint({
-  element: document.getElementById('check'),
-  handler: function() {
-    $('.skillbar').each(function(){
-      $(this).find('.skillbar-bar').animate({
-        width:$(this).attr('data-percent')
-      },4000);
-    });
+
+
+
+$( document ).ready(function() {
+
+  if ( $('#nonexistent').length) {
+    // Correct! This code will only run if there's an element in your page
+    // with an ID of 'nonexistent'
   }
-})
+
+  $( '<p class="greet">Hello!</p>' ); // creates a new <p> with content and class
+
+  $( '.class' ).html("hello"); // changes the html of something
+
+
+  // select all list items
+  var listItems = $( 'li' );
+
+  // filter the selection to only items with a class of 'special'
+  var special = listItems.filter( '.special' );
+
+  // filter the selection to only items without a class of 'special'
+  var notSpecial = listItems.not( '.special' );
+
+  // filter the selection to only items that contain a span
+  var hasSpans = listItems.has( 'span' );
+
+  // change and set/remove classes
+  $( '#asdasd' ).addClass( 'hidden' );
+  $( '#asdasd' ).removeClass( 'hidden' );
+  $( '#asdasd' ).toggleClass( 'hidden' );
+
+  // change and set/remove html attributes
+  $( '#funny' ).attr( 'title', 'Beware' );
+  $("#funny").removeAttr( 'title' )
+
+  var input = $( 'input[type="number"]' );
+  input.val( 'new value' );
+  input.val(); // returns 'new value'
 
 
 
-
-
+});
 
 
 
